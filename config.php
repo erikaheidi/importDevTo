8c812dae1017c9f93fa9310b3decbc8cb8582c55
Erika Heidi <erika@erikaheidi.com>
2022-04-21 14:55:33 +0200

    Added action metadata, Dockerfile

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 return [
     'app_path' => __DIR__ . '/app/Command',
     'debug' => true,
-    'devto_username' => 'erikaheidi',
+    'devto_username' => getenv('DEVTO_USERNAME') ?: 'erikaheidi',
     'data_path' => __DIR__ . '/data'
 ];
